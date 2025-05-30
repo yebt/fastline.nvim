f88432661cb2e1381240aab5590d3095023c472d
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-30 16:34:46 -0500

    feat: add more hightlights

--- lua/fastline/highlights.lua
+++ lua/fastline/highlights.lua
@@ -7,10 +7,17 @@ function M.setup()
   hl(0, "FastlineInfo", { fg = "#55aaee" })
   hl(0, "FastlineArrow", { fg = "#ffaaee" })
 
+  hl(0, "FastlineFilename", { fg = "#abb2bf", bold = true })
+  hl(0, "FastlineFilenameR", { fg = "#abb2bf", reverse = true, bold = true })
+
   -- Mode-specific highlights
   hl(0, "FastlineModeNormal", { fg = "#98c379", bold = true })
   hl(0, "FastlineModeInsert", { fg = "#61afef", bold = true })
   hl(0, "FastlineModeVisual", { fg = "#c678dd", bold = true })
+
+  hl(0, "FastlineModeNormalR", { fg = "#98c379", reverse = true, bold = true })
+  hl(0, "FastlineModeInsertR", { fg = "#61afef", reverse = true, bold = true })
+  hl(0, "FastlineModeVisualR", { fg = "#c678dd", reverse = true, bold = true })
 end
 
 return M
