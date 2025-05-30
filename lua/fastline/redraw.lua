7f418acbe4ab59a9d01648d6af1fd2c09987f1a0
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-29 22:40:09 -0500

    feat add plugin

---
+++ lua/fastline/redraw.lua
@@ -0,0 +1,16 @@
+local M = {}
+local scheduled = false
+
+function M.schedule()
+  if scheduled then
+    return
+  end
+  scheduled = true
+
+  vim.schedule(function()
+    scheduled = false
+    vim.cmd("redrawstatus")
+  end)
+end
+
+return M
