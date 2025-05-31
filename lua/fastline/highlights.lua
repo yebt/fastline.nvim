639566c501ab97a75bece9c225a6909f34f9cb18
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-05-31 08:49:36 -0500

    feat: add hightlights

--- lua/fastline/highlights.lua
+++ lua/fastline/highlights.lua
@@ -2,11 +2,14 @@ local M = {}
 
 function M.setup()
   local hl = vim.api.nvim_set_hl
+  -- Base
+  
+  -- Modules
   hl(0, "FastlineSeparator", { fg = "#888888" })
   hl(0, "FastlineEncoding", { fg = "#ffaa00" })
   hl(0, "FastlineInfo", { fg = "#55aaee" })
+  hl(0, "FastlineGit",       { fg = "#ff6c6b", bold = true })
   hl(0, "FastlineArrow", { fg = "#ffaaee" })
-
   hl(0, "FastlineFilename", { fg = "#abb2bf", bold = true })
   hl(0, "FastlineFilenameR", { fg = "#abb2bf", reverse = true, bold = true })
 
@@ -19,6 +22,11 @@ function M.setup()
   hl(0, "FastlineModeInsertR", { fg = "#61afef", reverse = true, bold = true })
   hl(0, "FastlineModeVisualR", { fg = "#c678dd", reverse = true, bold = true })
 
+
+  hl(0, "FastlinGiteHead",   {})
+  hl(0, "FastlinGiteAdd",    {})
+  hl(0, "FastlinGiteChange", {})
+  hl(0, "FastlinGiteDelete", {})
 end
 
 return M
